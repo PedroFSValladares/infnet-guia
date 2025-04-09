ae8c95d496aae60a346af158e8568445c0d21fea
Pedro Valladares <pedrohvalladares@hotmail.com>
2025-04-09 19:24:43 -0300

    setup prometheus and grafana

---
+++ pages/api/metrics.js
@@ -0,0 +1,9 @@
+import client from 'prom-client';
+
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+export default async function handler(req, res) {
+    res.setHeader('Content-Type', register.contentType);
+    res.end(await register.metrics());
+}
